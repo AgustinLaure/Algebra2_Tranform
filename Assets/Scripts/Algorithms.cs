9fd55ee9e9653e68a0ec6904c6faa9c8441bb8d2
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-09-20 06:26:27 -0300

    Add Bubble, Cocktail & Quick sorts

--- Assets/Scripts/Algorithms.cs
+++ Assets/Scripts/Algorithms.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 
 public static class Algorithms
 {
+    #region Bitonic
     //O(n log^2(n)) costo computacional
     //O(log(n)) costo en memoria
-
-    #region Bitonic
-
 
     // direction == 1 ascendente
     // direction == -1 descendente
@@ -58,10 +56,9 @@ public static class Algorithms
 
     #endregion
 
+    #region SelectionSort
     //O(n^2) en costo computacional
     //O(1) en memoria
-    #region SelectionSort
-
 
     //Sortea guardando el indice del mas pequeño/grande para ponerlo en su posicion correspondiente
     public static void SelectionSort<T>(T[] array, int direction) where T : IComparable
@@ -83,6 +80,144 @@ public static class Algorithms
             }
 
             Swap<T>(array, i, lowestValueIndex);
+        }
+    }
+
+    #endregion
+
+    #region BubbleSort
+
+    //O(n^2) costo computacional
+    //O(1) costo en memoria
+
+    public static void BubbleSort<T>(T[] array, int direction) where T : IComparable
+    {
+        int dir = 1 * (int)Mathf.Sign(direction);
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            bool hasSwapped = false;
+
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                int comparison = array[j].CompareTo(array[j + 1]);
+
+                if (comparison * dir > 0)
+                {
+                    Swap(array, j, j + 1);
+                    hasSwapped = true;
+                }
+            }
+
+            if (!hasSwapped)
+            {
+                return;
+            }
+        }
+    }
+
+    #endregion
+
+    #region CocktailSort
+
+    //O(n^2) costo computacional
+    //O(1) costo en memoria
+    public static void CocktailSort<T>(T[] array, int direction) where T : IComparable
+    {
+        int dir = 1 * (int)Mathf.Sign(direction);
+        int start = 0;
+        int end = array.Length - 1;
+
+        bool hasSwapped = true;
+
+        while (hasSwapped)
+        {
+            hasSwapped = false;
+
+            for (int i = start; i < end; i++)
+            {
+                int comparison = array[i].CompareTo(array[i + 1]);
+
+                if (comparison * dir > 0)
+                {
+                    Swap(array, i, i + 1);
+                    hasSwapped = true;
+                }
+            }
+
+            if (!hasSwapped)
+            {
+                return;
+            }
+
+            hasSwapped = false;
+
+            end--;
+
+            for (int i = end; i > start; i--)
+            {
+                int comparison = array[i].CompareTo(array[i - 1]);
+
+                if (comparison * dir < 0)
+                {
+                    Swap(array, i, i - 1);
+                    hasSwapped = true;
+                }
+            }
+
+            start++;
+        }
+    }
+
+    #endregion
+
+    #region QuickSort
+
+    //O(n log(n)) en el mejor de los casos y 0(n^2) en el peor, cuando el mas grande o mas chico se selecciona siempre como pivote. costo computacional
+    //O(log(n)) costo memoria y O(n) en el peor de los casos. 
+
+    public static void QuickSortArray<T>(T[] array, int direction) where T : IComparable
+    {
+        int dir = 1 * (int)Mathf.Sign(direction);
+
+        QuickSort(array, 0, array.Length - 1, dir);
+    }
+
+    // mueve los valores alrededor del pivote, haciendo que los menores vayan de un lado y lo mayores de otro
+    private static int Divide<T>(T[] array, int low, int high, int direction) where T : IComparable
+    {
+        T pivot = array[high];
+
+        // la posicion de la 'frontera' del mas chico
+        int i = low - 1;
+
+        //mueve los elementos mas chicos y desplaza la frontera
+        for (int j = low; j <= high - 1; j++)
+        {
+            int comparison = array[j].CompareTo(pivot);
+            if (comparison * direction < 0)
+            {
+                i++;
+                Swap(array, i, j);
+            }
+        }
+
+        //mueve al pivot adelante de la frontera
+        Swap(array, i + 1, high);
+
+        return i + 1;
+    }
+
+    //ordena los mayores y menores al rededor del pivot de forma recursiva hasta que ya no hay mas que ordenar
+    private static void QuickSort<T>(T[] array, int low, int high, int direction) where T : IComparable
+    {
+        if (low < high)
+        {
+            int pivot = Divide(array, low, high, direction);
+
+            // ordena los elementos mayores y menores al pivot respectivamente
+            QuickSort(array, low, pivot - 1, direction);
+            QuickSort(array, pivot + 1, high, direction);
         }
     }
 
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 
 public class Tester : MonoBehaviour
 {
-    int[] foo = new int[]
+    int[] order = new int[]
     {
-        1,5,3
+        6,1,2,9,1,3,4,6
     };
 
     private void Start()
     {
-        Algorithms.SelectionSort(foo, -1);
+        int[] bitonic = order;
+        Algorithms.BitonicSortArray<int>(bitonic, 1);
+        Debug.Log(bitonic);
 
-        Debug.Log(foo);
+        int[] selectionSort = order;
+        Algorithms.SelectionSort<int>(selectionSort, 1);
+        Debug.Log(selectionSort);
+
+        int[] bubbleSort = order;
+        Algorithms.BubbleSort<int>(bubbleSort, 1);
+        Debug.Log(bubbleSort);
+
+        int[] cocktailSort = order;
+        Algorithms.CocktailSort<int>(cocktailSort, 1);
+        Debug.Log(cocktailSort);
+
+        int[] quickSort = order;
+        Algorithms.QuickSortArray<int>(quickSort, 1);
+        Debug.Log(quickSort);
     }
 }
